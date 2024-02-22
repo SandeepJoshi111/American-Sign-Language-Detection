863c70037f1c030fd1841da5123d8fbfc85b8094
Sandeep Joshi <joshisandip111@gmail.com>
2024-02-22 15:26:32 +0545

    added gesture for number and operation

---
+++ collect-data-num.py
@@ -0,0 +1,131 @@
+import cv2
+import numpy as np
+import os
+import string
+# Create the directory structure
+if not os.path.exists("datanum"):
+    os.makedirs("datanum")
+if not os.path.exists("datanum/train"):
+    os.makedirs("datanum/train")
+if not os.path.exists("datanum/test"):
+    os.makedirs("data/test")
+
+# Create directories for each class in training and testing
+for i in range(10):
+    if not os.path.exists("datanum/train/" + str(i)):
+        os.makedirs("datanum/train/"+str(i))
+    if not os.path.exists("datanum/test/" + str(i)):
+        os.makedirs("datanum/test/"+str(i))
+
+for item in ['add', 'sub', 'mul', 'div']:
+    if not os.path.exists("datanum/train/" + item):
+        os.makedirs("datanum/train/" + item)
+    if not os.path.exists("datanum/test/" + item):
+        os.makedirs("datanum/test/" + item)
+
+# Train or test 
+mode = 'test'
+directory = 'datanum/'+mode+'/'
+minValue = 70
+
+cap = cv2.VideoCapture(0)
+interrupt = -1  
+
+while True:
+    _, frame = cap.read()
+    # Simulating mirror image
+    frame = cv2.flip(frame, 1)
+    
+    # Getting count of existing images
+    count = {
+             'zero': len(os.listdir(directory+"/0")),
+             'one': len(os.listdir(directory+"/1")),
+             'two': len(os.listdir(directory+"/2")),
+             'three': len(os.listdir(directory+"/3")),
+             'four': len(os.listdir(directory+"/4")),
+             'five': len(os.listdir(directory+"/5")),
+             'six': len(os.listdir(directory+"/6")),
+             'seven': len(os.listdir(directory+"/7")),
+             'eight': len(os.listdir(directory+"/8")),
+             'nine': len(os.listdir(directory+"/9")),
+             'add': len(os.listdir(directory+"/add")),
+             'sub': len(os.listdir(directory+"/sub")),
+             'mul': len(os.listdir(directory+"/mul")),
+             'div': len(os.listdir(directory+"/div")),
+             }
+    
+    # Printing the count in each set to the screen
+    cv2.putText(frame, "MODE : " + mode, (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 1)
+    cv2.putText(frame, "ZERO : "+str(count['zero']), (10, 70), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "ONE : "+str(count['one']), (10, 80), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "TWO : "+str(count['two']), (10, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "THREE : "+str(count['three']), (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "FOUR : "+str(count['four']), (10, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "FIVE : "+str(count['five']), (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "SIX : "+str(count['six']), (10, 130), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "SEVEN : "+str(count['seven']), (10, 140), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "EIGHT : "+str(count['eight']), (10, 150), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "NINE : "+str(count['nine']), (10, 160), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "ADD : "+str(count['add']), (10, 170), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "SUB : "+str(count['sub']), (10, 180), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "MUL : "+str(count['mul']), (10, 190), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "DIV : "+str(count['div']), (10, 200), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    # Coordinates of the ROI
+    x1 = int(0.5*frame.shape[1])
+    y1 = 10
+    x2 = frame.shape[1]-10
+    y2 = int(0.5*frame.shape[1])
+    # Drawing the ROI
+    # The increment/decrement by 1 is to compensate for the bounding box
+    cv2.rectangle(frame, (220-1, 9), (620+1, 419), (255,0,0) ,1)
+    # Extracting the ROI
+    roi = frame[10:410, 220:520]
+#    roi = cv2.resize(roi, (64, 64))
+    
+    cv2.imshow("Frame", frame)
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    
+    blur = cv2.GaussianBlur(gray,(5,5),2)
+    #blur = cv2.bilateralFilter(roi,9,75,75)
+    
+    th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+    ret, test_image = cv2.threshold(th3, minValue, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+
+    
+    test_image = cv2.resize(test_image, (300,300))
+    cv2.imshow("test", test_image)
+        
+    interrupt = cv2.waitKey(10)
+    if interrupt & 0xFF == 27: # esc key
+        break
+    if interrupt & 0xFF == ord('0'):
+        cv2.imwrite(directory+'0/'+str(count['zero'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('1'):
+        cv2.imwrite(directory+'1/'+str(count['one'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('2'):
+        cv2.imwrite(directory+'2/'+str(count['two'])+'.jpg', roi)       
+    if interrupt & 0xFF == ord('3'):
+        cv2.imwrite(directory+'3/'+str(count['three'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('4'):
+        cv2.imwrite(directory+'4/'+str(count['four'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('5'):
+        cv2.imwrite(directory+'5/'+str(count['five'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('6'):
+        cv2.imwrite(directory+'6/'+str(count['six'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('7'):
+        cv2.imwrite(directory+'7/'+str(count['seven'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('8'):
+        cv2.imwrite(directory+'8/'+str(count['eight'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('9'):
+        cv2.imwrite(directory+'9/'+str(count['nine'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('+'):
+        cv2.imwrite(directory+'add/'+str(count['add'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('-'):
+        cv2.imwrite(directory+'sub/'+str(count['sub'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('*'):
+        cv2.imwrite(directory+'mul/'+str(count['mul'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('/'):
+        cv2.imwrite(directory+'div/'+str(count['div'])+'.jpg', roi)
+cap.release()
+cv2.destroyAllWindows()
---
+++ see-num.py
@@ -0,0 +1,183 @@
+import cv2
+import numpy as np
+from keras.models import model_from_json
+from keras.preprocessing import image
+
+# Load the saved model architecture from JSON file
+model_path = "model-num/model-num.json"
+with open(model_path, 'r') as json_file:
+    loaded_model_json = json_file.read()
+
+# Create the model from the loaded JSON
+loaded_model = model_from_json(loaded_model_json)
+
+# Load weights into the model
+loaded_model.load_weights("model-num/model-num.h5")
+print(loaded_model.input_shape)
+
+# Compile the model
+loaded_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Load class names from label.txt
+label_path = "model-num/label-num.txt"
+with open(label_path, "r") as label_file:
+    class_names = label_file.read().splitlines()
+
+# Set up the video capture
+cap = cv2.VideoCapture(0)
+
+# Define the ROI parameters
+roi_size = 300
+frame_size = 900
+
+# Initialize counters
+symbol_counters = {label: 0 for label in class_names}
+word = ""
+sentence = ""
+
+blank_counter = 0
+
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    # Resize the frame to 900x900
+    frame_resized = cv2.resize(frame, (frame_size, frame_size))
+
+    # Flip the frame horizontally
+    frame_resized = cv2.flip(frame_resized, 1)
+
+    # Create a black frame for the ROI
+    roi_frame = np.zeros((frame_size, frame_size, 3), dtype=np.uint8)
+
+    # Copy the flipped webcam frame to the ROI region
+    roi_frame[0:roi_size, frame_size-roi_size:frame_size, :] = frame_resized[0:roi_size, 0:roi_size, :]
+
+    # Draw a border around the ROI
+    cv2.rectangle(frame_resized, (frame_size-roi_size, 0), (frame_size, roi_size), (0, 255, 0), 2)
+
+    # Create a white box inside frame_resized
+    box_height = 200
+    cv2.rectangle(frame_resized, (0, frame_size - box_height), (frame_size, frame_size), (255, 255, 255), -1)
+
+    # Extract the ROI for prediction
+    roi = frame_resized[0:roi_size, frame_size-roi_size:frame_size, :]
+
+    # Convert the ROI to grayscale
+    roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+    # Apply Gaussian blur
+    blur = cv2.GaussianBlur(roi_gray, (5, 5), 2)
+
+    # Apply adaptive thresholding
+    th3 = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    kernel = np.ones((2, 2), np.uint8)
+    th3 = cv2.erode(th3, kernel, iterations=1)
+    th3 = cv2.dilate(th3, kernel, iterations=1)
+    ret, res = cv2.threshold(th3, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    # Display the frame with ROI and predicted class label
+    cv2.imshow('Thresholded ROI', res)
+
+    # Make predictions
+    img = cv2.resize(res, (128, 128))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0
+    predictions = loaded_model.predict(img_array)
+
+    # Get the predicted class index
+    predicted_class_index = np.argmax(predictions[0])
+
+    # Get the predicted class label
+    predicted_class_label = class_names[predicted_class_index]
+
+    # Increment the symbol counter
+    symbol_counters[predicted_class_label] += 1
+
+    # Display the predicted class label inside the white box
+    cv2.putText(frame_resized, f"Predicted Sign: {predicted_class_label}", (10, frame_size - int(box_height) + 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+    print(symbol_counters[predicted_class_label])
+    # Update the word if the count is greater than 30
+    # Update the word if the count is greater than 30
+    if symbol_counters[predicted_class_label] >= 50:
+        word += predicted_class_label
+        # Check if the word contains 'mul' for multiplication
+        if 'mul' in word:
+            # Extract numeric labels before and after 'mul'
+            parts = word.split('mul')
+            if len(parts) == 2 and parts[0].isdigit() and parts[1].isdigit():
+                operand1 = int(parts[0])
+                operand2 = int(parts[1])
+                # Perform multiplication and update the word
+                result = operand1 * operand2
+                sentence = str(result)
+            else:
+                # Handle invalid 'mul' operation
+                print("Invalid operation: Missing operand")
+
+        # Check if the word contains 'add' for addition
+        elif 'add' in word:
+            parts = word.split('add')
+            if len(parts) == 2 and parts[0].isdigit() and parts[1].isdigit():
+                operand1 = int(parts[0])
+                operand2 = int(parts[1])
+                result = operand1 + operand2
+                sentence = str(result)
+            else:
+                print("Invalid operation: Missing operand")
+
+        # Check if the word contains 'div' for division
+        elif 'div' in word:
+            parts = word.split('div')
+            if len(parts) == 2 and parts[0].isdigit() and parts[1].isdigit() and int(parts[1]) != 0:
+                operand1 = int(parts[0])
+                operand2 = int(parts[1])
+                result = operand1 / operand2
+                sentence = str(result)
+            else:
+                print("Invalid operation: Missing or zero divisor")
+
+        # Check if the word contains 'sub' for subtraction
+        elif 'sub' in word:
+            parts = word.split('sub')
+            if len(parts) == 2 and parts[0].isdigit() and parts[1].isdigit():
+                operand1 = int(parts[0])
+                operand2 = int(parts[1])
+                result = operand1 - operand2
+                sentence = str(result)
+            else:
+                print("Invalid operation: Missing operand")
+        for label in class_names:
+            symbol_counters[label] = 0   # Reset the counter after updating the word
+
+
+    # Increment blank frame counter if predicted class label is 'blank'
+    if predicted_class_label == "blank":
+        blank_counter += 1
+    else:
+        blank_counter = 0
+
+    # Check if consecutive blank frames are reached
+    if blank_counter >= 30:
+        # Reset word, sentence, and symbol counters after 20 consecutive blank frames
+        word = ""
+        sentence = ""
+        for label in class_names:
+            symbol_counters[label] = 0  
+
+    # Display the live webcam feed with predicted class label, word, and sentence
+    cv2.putText(frame_resized, f"Word: {word}", (10, frame_size - int(box_height) + 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    cv2.putText(frame_resized, f"Result: {sentence}", (10, frame_size - int(box_height) + 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    cv2.imshow('Live Webcam Feed with Prediction', frame_resized)
+
+    # Display the predicted class label
+    print(f"Predicted Class: {predicted_class_label}")
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
